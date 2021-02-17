e03f76e203c9ba669f4042dd08fcf34d92fff3ec
kvnptl <kevinpatel4400@gmail.com>
2021-02-17 18:52:16 +0000

    cmd vel publisher

--- src/my_robot/src/safety_detector_node.cpp
+++ src/my_robot/src/safety_detector_node.cpp
@@ -2,25 +2,53 @@
 #include "geometry_msgs/Twist.h"
 #include "sensor_msgs/LaserScan.h"
 
-// Initialize the node
-ros::init(argc, argv, "safety_detector_node");
-ros::NodeHandle n;
-
-ros::Rate r(1);
-ros::Publisher drive_pub = n.advertise<geometry_msgs::Twist>("cmd_vel", 1);
+bool send_stop = false;
 
 void Callback(const sensor_msgs::LaserScan msg)
 {
-  ROS_INFO("I heard: [%s]", msg);
-//   drive_pub.publish(marker);
+  ROS_INFO("I heard");
+  send_stop = true;
+
 }
 
 
 int main(int argc, char** argv){
+    // Initialize the node
+    ros::init(argc, argv, "safety_detector_node");
+    ros::NodeHandle n;
+    ros::Rate r(1);
 
-  ros::Subscriber sub = n.subscribe("scan", 1000, Callback);
-  
-  ros::spin();
+    ros::Publisher drive_pub = n.advertise<geometry_msgs::Twist>("cmd_vel", 1);
+    ros::Subscriber laser_data = n.subscribe("scan", 1000, Callback);
+    
+    if (send_stop == true){
+        geometry_msgs::Twist data1;
 
-  return 0;
+        data1.linear.x = 0;
+        data1.linear.y = 0;
+        data1.linear.z = 0;
+
+        data1.angular.x = 0;
+        data1.angular.y = 0;
+        data1.angular.z = 0;
+
+        drive_pub.publish(data1);
+    }
+    else{
+        geometry_msgs::Twist data2;
+
+        data2.linear.x = 0.15;
+        data2.linear.y = 0;
+        data2.linear.z = 0;
+
+        data2.angular.x = 0;
+        data2.angular.y = 0;
+        data2.angular.z = 0;
+
+        drive_pub.publish(data2);
+    }
+
+    ros::spin();
+
+    return 0;
 }
